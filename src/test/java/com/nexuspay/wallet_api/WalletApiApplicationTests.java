d60774beb005c9a084b31c70cd2b552e819b78dc
Tomas Garcia <tomas.garcia.col17@gmail.com>
2026-03-18 20:31:33 -0300

    chore: initial spring boot project structure

---
+++ src/test/java/com/nexuspay/wallet_api/WalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexuspay.wallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
